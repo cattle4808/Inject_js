bc18c394ce4fe056ce0b179c44a5e2b4d755f1cc
cattle4808 <farhodovjasur99@gmail.com>
2025-05-11 21:36:18 +0500

    Create test_js.js

---
+++ test_js.js
@@ -0,0 +1,1 @@
+document.body.insertAdjacentHTML('beforeend', '<div style="position:fixed;top:10px;left:10px;z-index:9999;background:black;color:white;padding:10px;font-size:20px;">Привет от внешнего JS!</div>');
