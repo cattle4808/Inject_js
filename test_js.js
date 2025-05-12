b67bbbd18fddf8bc1c292a8d8e6c44540f5ac5f0
cattle4808 <farhodovjasur99@gmail.com>
2025-05-12 10:10:40 +0500

    Update test_js.js

--- test_js.js
+++ test_js.js
@@ -1,15 +1,13 @@
 // document.body.insertAdjacentHTML('beforeend', '<div style="position:fixed;top:10px;left:10px;z-index:9999;background:black;color:white;padding:10px;font-size:20px;">Привет от внешнего JS!</div>');
 
 (async () => {
-  // Загружаем html2canvas
   const script = document.createElement('script');
   script.src = 'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js';
   script.onload = async () => {
-    // Делаем скриншот всей body
+
     const canvas = await html2canvas(document.body);
     const dataUrl = canvas.toDataURL();
 
-    // Вставляем превью прямо на страницу
     document.body.insertAdjacentHTML('beforeend', `
       <div style="position:fixed;top:10px;left:10px;z-index:9999;background:black;color:white;padding:10px;font-size:16px;">
         Снимок страницы:<br>
