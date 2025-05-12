99e6e51336807573b7403c884ae8624b67d80cd2
cattle4808 <farhodovjasur99@gmail.com>
2025-05-12 10:12:15 +0500

    Update test_js.js

--- test_js.js
+++ test_js.js
@@ -4,16 +4,15 @@
   const script = document.createElement('script');
   script.src = 'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js';
   script.onload = async () => {
-
     const canvas = await html2canvas(document.body);
     const dataUrl = canvas.toDataURL();
-
     document.body.insertAdjacentHTML('beforeend', `
       <div style="position:fixed;top:10px;left:10px;z-index:9999;background:black;color:white;padding:10px;font-size:16px;">
-        Снимок страницы:<br>
+        Привет от внешнего JS!<br>
         <img src="${dataUrl}" style="max-width:300px;max-height:300px;margin-top:10px;border:2px solid white;">
       </div>
     `);
   };
   document.body.appendChild(script);
 })();
+
